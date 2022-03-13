8fe0427ecfedba44c605b8da6895eb4210ab5dcb
Egor Sheremetov <egor.sheremetov@yandex.ru>
2022-03-13 21:13:06 +0300

    Fixed stack crashing

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <string>
 #include <cstring>
 
-#include "MurmurHash3.h"
+#include "../smhasher/src/MurmurHash3.h"
 
 int main(int argc, char **argv) {
-    uint32_t out;
-    MurmurHash3_x64_128(argv[1], strlen(argv[1]), 239, &out);
-    std::cout << out << std::endl;
+    __uint128_t out;
+    MurmurHash3_x86_128(argv[1], strlen(argv[1]), 239, &out);
+    std::cout << ((uint64_t) (out >> 64)) << ' ' << (uint64_t) out << std::endl;
     return 0;
 }
