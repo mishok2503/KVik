2660b115cf3f28a35be05aa01abfb6a224d22767
Egor Sheremetov <egor.sheremetov@yandex.ru>
2022-03-13 20:35:07 +0300

    Created CmakeLists and basic project structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 
 #include "MurmurHash3.h"
 
 int main(int argc, char **argv) {
-
-    std::string str = argv[1];
     uint32_t out;
-
-    MurmurHash3_x64_128(str.data(), str.size(), 239, &out);
-
+    MurmurHash3_x64_128(argv[1], strlen(argv[1]), 239, &out);
     std::cout << out << std::endl;
-
     return 0;
 }
