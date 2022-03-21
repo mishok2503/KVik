802298843a9e97aa6b8c142650cee3f39b6de71c
Ilya <ilya.brovarnik@gmail.com>
2022-03-22 00:12:03 +0300

    more results

--- get_data.sh
+++ get_data.sh
@@ -3,6 +3,6 @@ make -j4
 for test_num in {1..5}
 do
     echo "Test $test_num: "
-    time "./benchmarks" "artifacts/res$test_num" 0
+    time "./cmake-build-release/benchmarks" "artifacts/res$test_num" 0
     echo ""
 done
